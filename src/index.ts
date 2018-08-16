ce4a7174a272b456c11bc02e0b2ef3cd0a6cc01a
jeremy woo <jeremy.woo@akqa.com>
2018-08-17 07:08:50 +1200

    added routine to work with sql api cosmos db.

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,31 @@
 import { CosmosClient } from "@azure/cosmos";
  
-const endpoint = "https://localhost:8081";                     // Add your endpoint
+const endpoint = "https://localhost:8081";   // Add your endpoint
 const masterKey = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";  // Add the masterkey of the endpoint
+
 const client = new CosmosClient({endpoint, auth: { masterKey }});
-
-const databaseDefinition = { id: "sampledatabase" };
-const collectionDefinition = { id: "samplecollection" };
-const itemDefinition = { id: "hello world doc", content: "Hello World!" };
-
+// Notice database variables are called one after another //
 async function helloCosmos() {
-      
-    const database = await client.database("databaseDefinition");
-    console.log(database);
- 
-    //console.log('created db');
+         
+    const dbResponse = await client.databases.createIfNotExists({
+       id : "TestDemo"
+    });
+
+    let database = dbResponse.database;
    
+    const coResponse = await database.containers.createIfNotExists({
+      id: "DummyTestCollection"
+    });
 
-   // const { database: db } = await client.database.(databaseDefinition);
-   // console.log('created db');
- 
-  //  const { container } = await db.container.create(collectionDefinition);
-  //  console.log('created collection');
- 
-   // const { body } = await container.items.create(documentDefinition);
-   // console.log('Created item with content: ', body.content);
- 
-   // await db.delete();
-   // console.log("Deleted database");
+    let container = coResponse.container;
+    console.log("Setting up the container...done!");
+    
+    // add item to it // 
+
+    const { body : doc } = await container.items.create({
+        'name' : 'jeremy', 
+        'password' : 'uGuessedit'
+    })    
 }
-
 
 helloCosmos().catch((err)=>{console.error(err)});
